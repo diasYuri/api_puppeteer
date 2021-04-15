bade58bb66c66fb099bf3e2e023e2bf1dda06d39
diasYuri <yuri@pop-os.localdomain>
2021-04-15 17:46:41 -0300

    feat: selecting book

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import { RandomValue } from "./helpers/randomValue.js";
-import amazonApi from "./service/amazon-api.js";
+//import amazonApi from "./service/amazon-api.js";
+import mockApi from "./service/mock.js";
 
 const URL_img = "https://images-na.ssl-images-amazon.com/images/I/";
+const URL_end = "._AC_UL1000.jpg";
 
-const HandleApi = async () => {
-  const books = await amazonApi();
-
-  const randomValue = RandomValue(books.length);
-  console.log(books[randomValue]);
+const handleSrc = (src) => {
+  const id_src = src.replace(URL_img, "").split(".")[0];
+  return URL_img + id_src + URL_end;
 };
 
-HandleApi();
+const handleBook = async () => {
+  const bookList = mockApi(); //await amazonApi();
+
+  const rValue = RandomValue(bookList.length);
+  let bookSelect = bookList[rValue];
+
+  bookSelect = {
+    src: handleSrc(bookSelect.src),
+    alt: bookSelect.alt,
+  };
+
+  console.log(bookSelect);
+};
+
+function init() {
+  handleBook();
+}
+
+init();
