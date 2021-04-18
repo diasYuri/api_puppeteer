ff985b47d5c48e5bd4c484e11b172606d47eab54
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-18 19:47:37 -0300

    fix: issues

--- src/service/amazon-api.js
+++ src/service/amazon-api.js
@@ -2,31 +2,35 @@
 import puppeteer from 'puppeteer'
 
 export default async function amazonApi () {
-  const browser = await puppeteer.launch({
-    headless: true,
-    // executablePath: "/usr/bin/chromium-browser",
-    args: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
-  })
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+      // executablePath: "/usr/bin/chromium-browser",
+      args: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']
+    })
 
-  const page = await browser.newPage()
-  await page.goto('https://www.amazon.com.br/gp/bestsellers/books/', {
-    timeout: 0,
-    waitUntil: 'networkidle0'
-  })
+    const page = await browser.newPage()
+    await page.goto('https://www.amazon.com.br/gp/bestsellers/books/', {
+      timeout: 0,
+      waitUntil: 'networkidle0'
+    })
 
-  const books = await page.evaluate(() => {
-    const ElemtList = document.querySelectorAll('ol li img')
+    const books = await page.evaluate(() => {
+      const ElemtList = document.querySelectorAll('ol li img')
 
-    const bookList = [...ElemtList]
+      const bookList = [...ElemtList]
 
-    const booksResponse = bookList.map(({ src, alt }) => ({
-      src,
-      alt
-    }))
+      const booksResponse = bookList.map(({ src, alt }) => ({
+        src,
+        alt
+      }))
 
-    return booksResponse
-  })
+      return booksResponse
+    })
 
-  await browser.close()
-  return books
+    await browser.close()
+    return books
+  } catch (error) {
+    throw new Error(error)
+  }
 }
